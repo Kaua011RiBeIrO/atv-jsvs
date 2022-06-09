5badcb5edcca1b4331a14555703fc9701ebcf91e
Kaua011RiBeIrO <102737682+Kaua011RiBeIrO@users.noreply.github.com>
2022-06-09 11:07:22 -0300

    Add files via upload

---
+++ exercicios2.java
@@ -0,0 +1,47 @@
+///////  atv01  //////
+
+/*import java.util.Scanner;
+
+public class exercicios2 {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+
+    System.out.println("Digite um numero: ");
+    double nota1 = entrada.nextDouble();
+    System.out.println(" " + nota1);
+
+    
+    System.out.println("Digite outro numero: ");
+    double nota2 = entrada.nextDouble();
+    System.out.println("" + nota1+nota2);
+
+    double soma = (nota1 + nota2);
+    System.out.println("Sua soma foi : " + soma);
+    }
+}*/
+
+///////    atv02     ///////
+
+/*import java.util.Scanner;
+
+public class exercicios2 {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite um valor em metros: ");
+        double valor = entrada.nextDouble();
+        System.out.println(" " + valor);
+
+    
+        double resul = ((valor)/100);
+        System.out.println("Em centimetro deu  : " + resul);
+    
+    }
+}*/
+
+//////  atv03   ///////
+
+
+
+
